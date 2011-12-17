246829cfac566c2e7910a95fabe02aef60781690
Wesley Hartford <github@cutterslade.ca>
2011-12-17 08:26:42 -0800

    initial project structure

---
+++ src/main/java/ca/cutterslade/securerest/App.java
@@ -0,0 +1,13 @@
+package ca.cutterslade.securerest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
